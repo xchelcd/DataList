d13b189abc5a6440387900944b423a5f53b5485f
xchelcd <xchel.co@gmail.com>
2023-11-04 19:00:26 -0600

    added mainState and mainViewModel

--- app/src/main/java/com/idaxmx/myapplication/MainState.kt
+++ app/src/main/java/com/idaxmx/myapplication/MainState.kt
@@ -1,5 +1,9 @@
 package com.idaxmx.myapplication
 
 data class MainState(
-    val list: List<User> = emptyList()
-)
+    val list: List<User> = emptyList(),
+    val filter: String = ""
+) {
+    fun updateFilter(filter: String) = copy(filter = filter)
+    fun updateList(list: List<User>) = copy(list = list)
+}
---
+++ app/src/main/java/com/idaxmx/myapplication/MainViewModel.kt
@@ -0,0 +1,42 @@
+package com.idaxmx.myapplication
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+
+
+class MainViewModel(
+    private val primaryList: List<User>
+): ViewModel() {
+
+    private var _state = MutableStateFlow(MainState())
+    val state: StateFlow<MainState> get() = _state
+
+    init {
+        setList(primaryList)
+    }
+
+    private fun filterList() {
+        val state = _state.value
+
+        val newList = if (state.filter.isBlank()) primaryList
+        else state.list.filter(::filter)
+        setList(newList)
+    }
+
+    private fun filter(user: User): Boolean = user.name.startsWith(_state.value.filter, true)
+
+    fun setFilter(filter: String) {
+        _state.update {
+            it.updateFilter(filter)
+        }
+        filterList()
+    }
+
+    private fun setList(list: List<User>) {
+        _state.update {
+            it.updateList(list)
+        }
+    }
+}
