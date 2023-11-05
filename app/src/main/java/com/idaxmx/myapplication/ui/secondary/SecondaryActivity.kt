42257a6a15b40a6840268b82df9efae38f893da7
xchelcd <xchel.co@gmail.com>
2023-11-04 19:16:21 -0600

    added new activity

---
+++ app/src/main/java/com/idaxmx/myapplication/ui/secondary/SecondaryActivity.kt
@@ -0,0 +1,12 @@
+package com.idaxmx.myapplication.ui.secondary
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.idaxmx.myapplication.R
+
+class SecondaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secondary)
+    }
+}
