9775046b9154d6f9e35d7353c6b432eb0e4867be
xchelcd <xchel.co@gmail.com>
2023-11-04 19:01:59 -0600

    added handlers

--- app/src/main/java/com/idaxmx/myapplication/MainActivity.kt
+++ app/src/main/java/com/idaxmx/myapplication/MainActivity.kt
@@ -1,23 +1,49 @@
 package com.idaxmx.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.idaxmx.myapplication.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
+    private var viewModel: MainViewModel = MainViewModel(
+        listOf(
+            User("123", "Xchel"),
+            User("123", "Alonso"),
+            User("123", "Octavio"),
+            User("123", "Antonio"),
+            User("123", "Darios"),
+            User("123", "Isaac"),
+            User("123", "Eduardo"),
+            User("123", "Jose"),
+            User("123", "Ian"),
+        )
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.state = MainState(
-            list = listOf(
-                User("123", "Xchel")
-            )
-        )
+        with(binding) {
+            setHandleUserSelected(::handleUserSelected)
+            setHandleFilter(viewModel::setFilter)
+        }
+        lifecycleScope.launch {
+            viewModel.state.collect(::render)
+        }
+    }
+
+    private fun handleUserSelected(user: User) {
+        Toast.makeText(this, user.name, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun render(state: MainState) {
+        binding.state = state
     }
 }
