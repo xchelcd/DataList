e7e3a6e5e25975d8449bc36c571409b29748a2ae
xchelcd <xchel.co@gmail.com>
2023-11-04 19:28:25 -0600

    added todo list

---
+++ app/src/main/java/com/idaxmx/myapplication/extra/Todo.kt
@@ -0,0 +1,9 @@
+package com.idaxmx.myapplication.extra
+
+/**
+ * TODO
+ * navigate from mainActivity to secondaryActivity but the secondaryActivity will display all user's data selected
+ * create a view for activity_secondary with the user data (be creative, so add more attributes to user object)
+ * for the future, User data will have at least photo: String (think about it, because the secondaryActivity will need more information about the user)
+ * improve activity_main ui
+ */
