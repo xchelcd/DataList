0af074388290d46ad25651b85d9eee2f8a58f55f
xchelcd <xchel.co@gmail.com>
2023-11-11 14:37:21 -0600

    new task to add user

--- app/src/main/java/com/idaxmx/myapplication/extra/Todo.kt
+++ app/src/main/java/com/idaxmx/myapplication/extra/Todo.kt
@@ -6,4 +6,5 @@ package com.idaxmx.myapplication.extra
  * create a view for activity_secondary with the user data (be creative, so add more attributes to user object)
  * for the future, User data will have at least photo: String (think about it, because the secondaryActivity will need more information about the user)
  * improve activity_main ui
+ * add new user return it at the mainActivity (update the list). Note: there is not data persistence, so the user will disappear if the app is killed
  */
